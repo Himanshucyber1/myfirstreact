47c81b3167e0719781f2de5e5bb7beffa6927bfb
Your Name <you@example.com>
2023-10-06 03:22:09 -0700

    hvugu commit

--- src/Counter.js
+++ src/Counter.js
@@ -20,13 +20,8 @@ export default function Counter() {
     <div>
    {num}
     </div>
-<<<<<<< HEAD
     <button onClick={Addition}>Add</button>
     <button onClick={Sub}>Subtract</button>
-=======
-    <button onClick={Additon}>Add</button>
-    <button onClick={Subtraction}>Subtract</button>
->>>>>>> e5540d380201da75ecf9746099c8601e74421424
     <button onClick={Multiply}>Mul</button>
     <button onClick={Divide}>Divide</button>
   </>
